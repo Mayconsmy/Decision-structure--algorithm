e8023d9986be5cd6a9e46bb0568c270e101278f7
Maycon Soares Maia <160624724+Mayconsmy@users.noreply.github.com>
2024-08-30 21:37:20 -0300

    Update notaaluno.c v2

--- notaaluno.c
+++ notaaluno.c
@@ -1,40 +1,39 @@
 #include <stdio.h>
 #include <locale.h>
 
-int main()
-{
-	setlocale(LC_ALL,"portuguese");
-	float MP,MF,A1,A2,A3,AF;
-	printf("-----------------------------");
-	printf("\nInforme a sua primeira nota: ");
-	scanf("%f",&A1);
-	printf("\nInforme a sua segunda nota: ");
-	scanf("%f",&A2);
-	printf("\nInforme a sua terceira nota: ");
-	scanf("%f",&A3);
-	printf("\n-----------------------------\n");
+int main(){
+setlocale(LC_ALL,"Portuguese");
+float MP, MF, A1, A2, A3, AF;
+printf("-----------------------------");
+printf("\nInforme a sua primeira nota: ");
+scanf("%f", &A1);
+printf("\nInforme a sua segunda nota: ");
+scanf("%f", &A2);
+printf("\nInforme a sua terceira nota: ");
+scanf("%f", &A3);
+printf("\n-----------------------------\n");
 
 	MP = (A1 + A2 + A3)/3;
+	printf("Média Parcial: %.1f\n", MP);
 	
-	if (MP >= 7.0){
-		printf("Aluno Aprovado!");
-		return (0);
+	if(MP >= 7.0){
+		printf("Aluno aprovado direto!");
 	}
-	else if (MP < 3.5){
-		printf("Aluno Reprovado!");
+	else if(MP < 3.5){
+		printf("Aluno reprovado direto!");
 	}
-	else if ((MP >= 3.5)&&(MP < 7.0)){
-		printf("Quarta prova! \n");
-		printf("Informe a Nota da avalia��o Final: ");
-		scanf("%f",&AF);
-		MF = ((6 * MP)+(4 * AF))/10;
-		if ((MP >= 7.0)||(MF >= 5.0)){
-			printf("\nAluno Aprovado!");
-		}
-		else{
-			printf("\nAluno Reprovado!");
-		}
+	else{
+		printf("\nQuarta prova! \n");
+		printf("Informe a nota da avaliação final: ");
+		scanf("%f", &AF);
+		MF = ((6 * MP) + (4 * AF)) / 10;
+		printf("Média Final: %.1f\n", MF);
+	if(MF >= 5.0){
+		printf("Aluno aprovado na final!");
 	}
-	
-	return (0);
+	else{
+		printf("Aluno reprovado na final!");
+	}
+	}
+	return(0);
 }
